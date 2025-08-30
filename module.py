1b625745674827bc8c9486426fcf881d4ad66578
DTH235625-VoThanhDat <datvo98760@gmail.com>
2025-08-30 07:26:48 +0700

    Chuong1

---
+++ module.py
@@ -0,0 +1,12 @@
+import math
+import os 
+
+import math_libs
+math_libs.add(4,5)
+
+import module1
+import module2
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
